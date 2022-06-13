40895fcb6f036c7b71f77106430977075eed2886
isabeels <101607831+isabeels@users.noreply.github.com>
2022-06-13 15:25:34 -0300

    estruturas iniciais

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          {/* element é o elemento para ser renderizado na primeira rota*/}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
