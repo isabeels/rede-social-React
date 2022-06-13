87de415d0dea0dbf1240e7d67dc245fb106acf0f
isabeels <101607831+isabeels@users.noreply.github.com>
2022-06-13 12:40:01 -0300

    testes na API

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import Api from "./Api";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-      <Api />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
